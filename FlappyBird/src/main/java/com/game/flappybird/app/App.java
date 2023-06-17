33957d42cb53ee3ce8da279cb75b0c0c40818908
Fandead <xhienkunlol1@gmail.com>
2023-06-17 23:16:31 +0700

    Update BestScore with Difficulty

--- FlappyBird/src/main/java/com/game/flappybird/app/App.java
+++ FlappyBird/src/main/java/com/game/flappybird/app/App.java
@@ -5,20 +5,25 @@
 package com.game.flappybird.app;
 
 import com.game.flappybird.util.Constant;
+import java.awt.HeadlessException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class App extends JPanel{
     public static void main(String[] args) {
-        JFrame frame = new JFrame(Constant.GAME_TITLE);
-        Game world = new Game();
-        frame.add(world);
-        frame.setSize(Constant.FRAME_WIDTH, Constant.FRAME_HEIGHT);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.pack();
-        frame.setVisible(true);
+        try {
+            JFrame frame = new JFrame(Constant.GAME_TITLE);
+            Game world = new Game();
+            frame.add(world);
+            frame.setSize(Constant.FRAME_WIDTH, Constant.FRAME_HEIGHT);
+            frame.setResizable(false);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLocationRelativeTo(null);
+            frame.pack();
+            frame.setVisible(true);
+        } catch (Exception e) {
+            
+        }
     }
 }
